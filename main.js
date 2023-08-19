44a701a4bf06dd8835bc6f46b35e1a71b734f60c
YusraCodey <120804906+YusraCodey@users.noreply.github.com>
2023-08-19 11:05:53 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var first_name="Ranbir";
+var last_name="Kapoor";
+first_name.concat(last_name);
+console.log(first_name.concat(last_name))
+var a= 10;
+var b= 5;
+var add=a+b;
+var sub=a-b;
+var mul=a*b;
+var div=a/b;
+console.log(add);
+console.log(sub);
+console.log(mul);
+console.log(div);
